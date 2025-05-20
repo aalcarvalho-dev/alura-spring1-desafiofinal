ee254844e9b579af316f15a45268aaed77a2aad8
aalcarvalho <aalcarvalho@NCELEPAR24498>
2025-05-20 09:30:34 -0300

    Primeiro commit

---
+++ src/main/java/com/alura/spring_desafiofinal/SpringDesafioFinalApplication.java
@@ -0,0 +1,13 @@
+package com.alura.spring_desafiofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesafioFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDesafioFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/spring_desafiofinal/SpringDesafioFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.spring_desafiofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesafioFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
